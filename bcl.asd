8c5a4cd48aeed1918a760d229f529293001c2b6f
g000001 <chiba.masaomi@gmail.com>
2021-06-21 16:35:37 +0900

    add: dispatch macrochar for regex #//

--- bcl.asd
+++ bcl.asd
@@ -4,11 +4,12 @@
   :description "Better CL"
   :author "CHIBA Masaomi <chiba.masaomi@gmail.com>"
   :license  "MIT"
-  :version "0.0.1"
+  :version "0.0.2"
   :serial t
   :depends-on (#:cl-ppcre #:series #:closer-mop #:nil-compat)
   :components ((:file "bcl")
                (:file "with")
                (:file "get")
                (:file "for")
+               (:file "regex")
                (:file "bcl-user")))
---
+++ regex.lisp
@@ -0,0 +1,43 @@
+;;; -*- mode :Lisp -*-
+
+(cl:in-package bcl.internal)
+
+
+(bcl:in-syntax bcl:*bcl*)
+
+
+(defun read-delimited-sequence (result-type endchar &optional (input-stream *standard-input*) recursivep cont)
+  (let* ((prev nil)
+         (ans (list nil))
+         (tem ans))
+    (do ((chr (read-char input-stream nil input-stream recursivep)
+              (read-char input-stream nil input-stream recursivep)))
+        ((or (eq chr input-stream)
+             (and (eql chr endchar)
+                  (not (eql #\\ prev))))
+         (values (coerce (cdr ans) result-type)
+                 (and cont (funcall cont input-stream))))
+      (setf (cdr tem)
+            (setq tem (list chr)))
+      (setq prev chr))))
+
+
+(bcl:eval-always
+  (flet ((read-/ (srm chr arg)
+           (declare (ignore arg))
+           (multiple-value-bind (re case-fold-p)
+                                (read-delimited-sequence 'string
+                                                         chr
+                                                         srm
+                                                         T
+                                                         (bcl:^ (in) 
+                                                           (case (peek-char nil in nil)
+                                                             ((#\i #\I) 
+                                                              (read-char in)
+                                                              :case-fold)
+                                                             (otherwise nil))))
+             (ppcre:create-scanner re :case-insensitive-mode case-fold-p))))
+    (set-dispatch-macro-character #\# #\/ #'read-/ bcl:*bcl*)))
+
+
+;;; *EOF*
